f58ab7b6c8b8c4f7210d269e6c68cc8ee3ef0327
aditi002 <aditi1chalisay@gmail.com>
2023-12-21 22:50:15 +0545

    important task page added

---
+++ src/pages/Important.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import TaskCard from "../components/TaskCard";
+
+const Important = ({ tasks }) => {
+    const [importantTasks, setImportantTasks] = useState([]);
+
+    useEffect(() => {
+        if (tasks) {
+            setImportantTasks(tasks.filter(task => {
+                return task.status === 'Incomplete';
+            }));
+        }
+    }, [tasks]);
+
+    return (
+        <>
+            <div className="header">
+                <div className="page-title">
+                    Important Tasks
+                </div>
+            </div>
+            <div>
+                <div className="task-section">
+                    {importantTasks.map((task) => (
+                        <TaskCard key={task.id} task={task} />
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Important;
